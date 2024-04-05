3a426cf88d5ae9e6ab7f716b992deb5ed04ebca5
Gustavo <gustavooki.dev@gmail.com>
2024-04-05 14:46:40 -0300

    Update testbench_03.py + Update terms.py

--- aulaOO/aula3/package/maths/terms.py
+++ aulaOO/aula3/package/maths/terms.py
@@ -16,36 +16,61 @@ class Reta():
 
         print(f'Os parâmetros do meu modelo de reta são: a={self.a}, b={self.b}')
 
-class Circle():
-    def __init__(self, r, x, y, color):
-        self.r = r
-        self.x = x
-        self.y = y
-        self.color = color
-
-
-    def circunferencia(self):
-        circunferencia = (2 * 3.14) * self.r
-        return circunferencia
-
-    def area(self):
-        area = 3.14 * pow(self.r, 2)
-        return area
-
-    def diametro(self):
-        diametro = self.r * 2
-        return diametro
-
-    def model(self):
-        print(f'Os parâmetros do meu modelo de circulo são: r={self.r}, x={self.x}, y={self.y} e cor={self.color}.')
 
 class Point():
-    def __init__(self, x, y, color, name):
-        self.x = x
-        self.y = y
-        self.color = color
-        self.name = name
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def setX(self, x):
+        self._x = x
+    
+    def setY(self, y):
+        self._y = y
+    
+    def getX(self):
+        return self._x
+    
+    def getY(self):
+        return self._y
+
+    def distance(self):
+        return(self._x**2 + self._y**2)**(0.5)
+    
+    def print(self):
+        print(f'Eu sou um ponto e minhas coordenadas são: x = {self._x} e y = {self._y}')
     
     def model(self):
         print(f'Os parâmetros do meu modelo de ponto são: coordenada=({self.x}, {self.y}), cor {self.color} e com o nome de P{self.name}')
-        
+
+class Circle(Point):
+    def __init__(self, x, y, raio):
+        super().__init__(x, y)
+        self._raio = raio
+
+    def print(self):
+        print(f'Eu sou um círculo e minhas coordenadas são: x = {self._x} e y = {self._y}. O meu raio é {self._raio}')
+
+# MEU CODIGO RASCUNHO
+# class Circle():
+#     def __init__(self, r, x, y, color):
+#         self.r = r
+#         self.x = x
+#         self.y = y
+#         self.color = color
+
+
+#     def circunferencia(self):
+#         circunferencia = (2 * 3.14) * self.r
+#         return circunferencia
+
+#     def area(self):
+#         area = 3.14 * pow(self.r, 2)
+#         return area
+
+#     def diametro(self):
+#         diametro = self.r * 2
+#         return diametro
+
+#     def model(self):
+#         print(f'Os parâmetros do meu modelo de circulo são: r={self.r}, x={self.x}, y={self.y} e cor={self.color}.')
--- aulaOO/aula3/testbench_03.py
+++ aulaOO/aula3/testbench_03.py
@@ -1,14 +1,9 @@
-from package.maths.terms import Point
+from package.maths.terms import Point, Circle
 
 def workspace():
-    ponto_1 = Point(5, 6, "vermelho", "A")
+    ponto_1 = Point(5, 6)
+    print(f'Distancia da origem = {ponto_1.distance()}')
     ponto_1.model()
-
-    ponto_2 = Point(12, 20, "verde", "B")
-    ponto_2.model()
-
-    distancia = pow((ponto_2.x - ponto_1.x), 2) + pow((ponto_2.y - ponto_1.y), 2)
-    print(f'A distancia entre os pontos P{ponto_1.name} e P{ponto_2.name} é de {distancia}cm.')
     
 
 if __name__ == "__main__":
